b28c70c32f9dabb36f4fcab7287696d19d2d51ee
Novuse <novuse@Novuses-MacBook-Pro.local>
2020-01-22 20:26:40 +0530

    .

---
+++ build/precache-manifest.1e72242eca25913284eb29d95f9bdebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0214be9d6b3fc1cd9bccaa66ffcbae66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8f60b1246b2b55b76c",
+    "url": "/static/css/main.73cabe67.chunk.css"
+  },
+  {
+    "revision": "72ea585db063a87105b0",
+    "url": "/static/js/2.d7cff241.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d7cff241.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c8f60b1246b2b55b76c",
+    "url": "/static/js/main.5c06d229.chunk.js"
+  },
+  {
+    "revision": "3ed401913923f15f7b4d",
+    "url": "/static/js/runtime-main.c1f408f5.js"
+  }
+]);
